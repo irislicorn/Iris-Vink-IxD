4627c7cc3ee6b3045feb3e377cff6f905e2625ff
Irislicorn <irislicorn@github.com>
2018-03-07 12:45:45 +0100

    hoi

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,33 @@
+function databalk() {
+  var elem = document.getElementById("databalk");   
+  var width = 99;
+  var id = setInterval(frame, 200);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width -= .1;
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+
+function databalk2() {
+  var elem = document.getElementById("databalk2");   
+  var width = 99;
+  var id = setInterval(frame, 400);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width -= .1;
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+
 function move() {
   var elem = document.getElementById("databalk");   
   var width = 99;
@@ -41,4 +71,97 @@ function heat() {
       elem.innerHTML = width + '°C';
     }
   }
+} 
+
+
+/*function oxygen() {
+  var format = d3.format(".4f");
+  console.log(' nu');
+  d3.select('#oxygen2')
+  .transition()
+  .duration(100000000)
+  .ease(d3.easeLinear)
+  .tween("text", function() {
+    var that = d3.select(this),
+    i = d3.interpolateNumber(that.text().replace(/,/g, ""), '20');
+    return function(t) {
+      that
+        .text(format(i(t)));
+    }
+  });
+  d3.select('#oxygen2balk')
+  .transition()
+  .duration(1000000000)
+  .ease(d3.easeLinear)
+  .style("width", '50px')
+
 }
+
+function oxygen2() {
+  var format = d3.format(".2f");
+  console.log(' nu');
+  d3.select('#oxygen1')
+  .transition()
+  .duration(1000000)
+  .ease(d3.easeLinear)
+  .tween("text", function() {
+    var that = d3.select(this),
+    i = d3.interpolateNumber(that.text().replace(/,/g, ""), '50');
+    return function(t) {
+      that
+        .text(format(i(t)));
+    }
+  });
+  d3.select('#oxygen1balk')
+  .transition()
+  .duration(10000000)
+  .ease(d3.easeLinear)
+  .style("width", '50px')
+
+}
+
+function fuel() {
+  var format = d3.format(".3f");
+  console.log(' nu');
+  d3.select('#fuelspan')
+  .transition()
+  .duration(1000000)
+  .ease(d3.easeLinear)
+  .tween("text", function() {
+    var that = d3.select(this),
+    i = d3.interpolateNumber(that.text().replace(/,/g, ""), '50');
+    return function(t) {
+      that
+        .text(format(i(t)));
+    }
+  });
+  d3.select('#fuel')
+  .transition()
+  .duration(10000000)
+  .ease(d3.easeLinear)
+  .style("width", '50px')
+
+}
+
+function celsius() {
+  var format = d3.format(".3f");
+  console.log(' nu');
+  d3.select('#celsius')
+  .transition()
+  .duration(1000000)
+  .ease(d3.easeLinear)
+  .tween("text", function() {
+    var that = d3.select(this),
+    i = d3.interpolateNumber(that.text().replace(/,/g, ""), '50');
+    return function(t) {
+      that
+        .text(format(i(t)));
+    }
+  });
+  d3.select('#heat')
+  .transition()
+  .duration(10000000)
+  .ease(d3.easeLinear)
+  .style("width", '50px')
+
+} */
